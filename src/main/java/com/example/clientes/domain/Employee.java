a59054ab2d349ef0ba2d257f951a69d8b6609763
CEIBA\pedro.pereira <ppereira@sura.com.co>
2024-04-16 14:45:22 -0500

    ok

---
+++ src/main/java/com/example/clientes/domain/Employee.java
@@ -0,0 +1,31 @@
+package com.example.clientes.domain;
+
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String email;
+}
---
+++ src/main/java/com/example/clientes/repository/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.example.clientes.repository;
+
+
+import com.example.clientes.domain.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    Optional<Employee> findByEmail(String email);
+}
